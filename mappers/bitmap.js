44a5cbc459b8918aa8ff8d8d2d1628810fe33b4b
Matt Komorowski <matt@mkomo.com>
2018-10-11 06:49:14 -0400

    added bitmap mapper

---
+++ mappers/bitmap.js
@@ -0,0 +1,23 @@
+function(d, f, debug) {
+  /**
+  f: function that maps input d to float value
+  colorScale: d3 scale to use (e.g. interpolateRdYlGn)
+  minmax: [min,...,max] values to anchor at 0 and 1, and equally spaced points between (everything below min or above max will also map to 0 or 1, resp)
+  scaleType: x will map to range in between the two closest values in minmax either linearly, log, or exp)
+  */
+  if (typeof f === 'object') {
+    debug = f.debug;
+    f = f.f;
+  }
+
+  let val = "" + f(d);
+  let color = '#' + val.padStart(6, '0');
+  if (debug) {
+    d.val = val;
+    d.color = color;
+    Object.keys(d).forEach(function(key) { if (['i', 't', 'val', 'color'].indexOf(key) < 0) delete d[key]; });
+  } else {
+    d.properties.fill = color;
+  }
+
+}
