bb9341bfb7c996c026d0c75688408690beb637a8
Matt Komorowski <matt@mkomo.com>
2019-10-11 06:53:35 -0400

    fix choropleth map

--- mappers/choropleth.js
+++ mappers/choropleth.js
@@ -2,10 +2,12 @@ function(d, f, colorScale, minmax, scaleType="linear", debug) {
   /**
   f: function that maps input d to float value
   colorScale: d3 scale to use (e.g. interpolateRdYlGn)
-  minmax: [min,...,max] values to anchor at 0 and 1, and equally spaced points between (everything below min or above max will also map to 0 or 1, resp)
-  scaleType: x will map to range in between the two closest values in minmax either linearly, log, or exp)
+  minmax: [min,...,max] values to anchor at 0 and 1, and equally spaced points between
+    (everything below min or above max will also map to 0 or 1, resp)
+  scaleType: TODO x will map to range in between the two closest values in minmax either linearly, log, or exp)
   */
   if (typeof f === 'object') {
+    // valid color scales can be found here: https://github.com/d3/d3-scale-chromatic
     colorScale = f.colorScale;
     minmax = f.minmax;
     scaleType = f.scaleType;
@@ -26,7 +28,7 @@ function(d, f, colorScale, minmax, scaleType="linear", debug) {
   } else if (i == n) {
     t = 1;
   } else {
-    t = (i*1.0)/n + (1.0/n) * (val - minmax[i - 1])/(minmax[i] - minmax[i - 1]);
+    t = (i - 1)/(n - 1) + (1/(n - 1)) * (val - minmax[i - 1])/(minmax[i] - minmax[i - 1]);
   }
 
   if (debug) {
